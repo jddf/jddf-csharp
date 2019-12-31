be66a7600c7c0744cd319d27bb357fa54defa54b
Ulysse Carion <ulysse@segment.com>
2019-12-31 14:25:00 -0800

    Temp commit, idk what I'm doing

---
+++ Jddf.Jddf/Start.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Jddf.Jddf
+{
+  public class Start
+  {
+    public int Add(int a, int b)
+    {
+      return a + b;
+    }
+
+    public int Sub(int a, int b)
+    {
+      return a - b;
+    }
+  }
+}
