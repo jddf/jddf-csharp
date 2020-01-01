92a04025c14bfb18523fe26ea5d2e8977714c74e
Ulysse Carion <ulysse@segment.com>
2019-12-31 17:37:35 -0800

    Use System.Text.Json instead of Newtonsoft

--- src/Jddf.Jddf/Discriminator.cs
+++ src/Jddf.Jddf/Discriminator.cs
@@ -1,29 +1,10 @@
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace Jddf.Jddf
 {
   public class Discriminator
   {
-    [JsonProperty("tag")]
     public string Tag { get; set; }
-
-    [JsonProperty("mapping")]
     public IDictionary<string, Schema> Mapping { get; set; }
-
-    public override bool Equals(object obj)
-    {
-      return obj is Discriminator discriminator &&
-             Tag == discriminator.Tag &&
-             EqualityComparer<IDictionary<string, Schema>>.Default.Equals(Mapping, discriminator.Mapping);
-    }
-
-    public override int GetHashCode()
-    {
-      var hashCode = -393007024;
-      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Tag);
-      hashCode = hashCode * -1521134295 + EqualityComparer<IDictionary<string, Schema>>.Default.GetHashCode(Mapping);
-      return hashCode;
-    }
   }
 }
--- test/Jddf.Jddf.Test/SchemaTest.cs
+++ test/Jddf.Jddf.Test/SchemaTest.cs
@@ -1,61 +1,49 @@
 using System.Collections.Generic;
 using Xunit;
-using Newtonsoft.Json;
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Jddf.Jddf.Test
 {
-  public class SchemaTest
-  {
-    [Fact]
-    public void TestingFromJson()
+    public class SchemaTest
     {
-      string json = @"{
-                    ""definitions"": { ""foo"": {}},
-                    ""ref"": ""foo"",
-                    ""type"": ""int8"",
-                    ""enum"": [""""],
-                    ""elements"": {},
-                    ""properties"": { ""foo"": {}},
-                    ""optionalProperties"": { ""foo"": {}},
-                    ""values"": {},
-                    ""discriminator"": { ""tag"": ""foo"", ""mapping"": {""foo"": {}}}
-                }";
+        [Fact]
+        public void SerializeDeserialize()
+        {
+            Schema schema = new Schema();
+            schema.Definitions = new Dictionary<string, Schema>() { { "foo", new Schema() } };
+            schema.Ref = "foo";
+            schema.Type = Type.Uint8;
+            schema.Enum = new HashSet<string>() { "" };
+            schema.Elements = new Schema();
+            schema.Properties = new Dictionary<string, Schema>() { { "foo", new Schema() } };
+            schema.OptionalProperties = new Dictionary<string, Schema>() { { "foo", new Schema() } };
+            schema.Values = new Schema();
+            schema.Discriminator = new Discriminator();
+            schema.Discriminator.Tag = "foo";
+            schema.Discriminator.Mapping = new Dictionary<string, Schema>() { { "foo", new Schema() } };
 
-      Schema schema = JsonConvert.DeserializeObject<Schema>(json);
+            var serializeOptions = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                IgnoreNullValues = true
+            };
+            serializeOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
 
-      Assert.Equal(new Dictionary<string, Schema>() { { "foo", new Schema() } }, schema.Definitions);
-      Assert.Equal("foo", schema.Ref);
-      Assert.Equal(Type.Int8, schema.Type);
-      Assert.Equal(new HashSet<string>() { "" }, schema.Enum);
-      Assert.Equal(new Schema(), schema.Elements);
-      Assert.Equal(new Dictionary<string, Schema>() { { "foo", new Schema() } }, schema.Properties);
-      Assert.Equal(new Dictionary<string, Schema>() { { "foo", new Schema() } }, schema.OptionalProperties);
-      Assert.Equal(new Schema(), schema.Values);
-      Assert.Equal("foo", schema.Discriminator.Tag);
-      Assert.Equal(new Dictionary<string, Schema>() { { "foo", new Schema() } }, schema.Discriminator.Mapping);
+            string serialized = JsonSerializer.Serialize(schema, serializeOptions);
+            Assert.Equal(
+                @"{""definitions"":{""foo"":{}},""ref"":""foo"",""type"":""uint8"",""enum"":[""""],""elements"":{},""properties"":{""foo"":{}},""optionalProperties"":{""foo"":{}},""values"":{},""discriminator"":{""tag"":""foo"",""mapping"":{""foo"":{}}}}",
+                serialized
+            );
+
+            // Comparing instances of Schema does not work, due to details of
+            // which instance of the various collections are instantiated.
+            // Instead, for this test we will satisfy ourselves with having the
+            // serialized representations be the same.
+            Schema deserialized = JsonSerializer.Deserialize<Schema>(serialized, serializeOptions);
+            string reserialized = JsonSerializer.Serialize(deserialized, serializeOptions);
+
+            Assert.Equal(serialized, reserialized);
+        }
     }
-
-    [Fact]
-    public void TestingToJson()
-    {
-      Schema schema = new Schema();
-      schema.Definitions = new Dictionary<string, Schema>() { { "foo", new Schema() } };
-      schema.Ref = "foo";
-      schema.Type = Type.Int8;
-      schema.Enum = new HashSet<string>() { "" };
-      schema.Elements = new Schema();
-      schema.Properties = new Dictionary<string, Schema>() { { "foo", new Schema() } };
-      schema.OptionalProperties = new Dictionary<string, Schema>() { { "foo", new Schema() } };
-      schema.Values = new Schema();
-      schema.Discriminator = new Discriminator();
-      schema.Discriminator.Tag = "foo";
-      schema.Discriminator.Mapping = new Dictionary<string, Schema>() { { "foo", new Schema() } };
-
-      Assert.Equal(
-          @"{""definitions"":{""foo"":{}},""ref"":""foo"",""type"":""int8"",""enum"":[""""],""elements"":{},""properties"":{""foo"":{}},""optionalProperties"":{""foo"":{}},""values"":{},""discriminator"":{""tag"":""foo"",""mapping"":{""foo"":{}}}}",
-          JsonConvert.SerializeObject(schema)
-        );
-    }
-  }
 }
